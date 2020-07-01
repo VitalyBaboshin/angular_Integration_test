7b48b85368062124866f961923be822b164dca2a
VitaliyBaboshin <vitaliy2@ro.ru>
2020-07-01 21:52:28 +0500

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-integration-tests';
+  title = 'angular-unit-test';
 }
